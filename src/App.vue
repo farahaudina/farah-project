d4df1b3e95b5984792733cab04eaae85fdafff48
farahaudina <farah06102006@gmail.com>
2024-03-21 15:36:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+ <h1>Hello World</h1> 
+
+  <Buttonc/>
+  <Buttonc/>
+  <TextH/>
+
+  </div>
+</template>  
+
+
+<script setup>
+import Buttonc from './components/Buttonc.vue'
+import TextH from './components/TextHeader.vue'
+</script>
---
+++ src/components/Buttonc.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn">
+        Submit
+    </button>
+</template>
+
+<style scoped>
+.btn{
+    background-color: aquamarine;
+    color: black;
+    font-size: 28px;
+}
+</style>
